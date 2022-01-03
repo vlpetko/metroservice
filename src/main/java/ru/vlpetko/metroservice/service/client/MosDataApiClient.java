f8f6971e4aaeafe16babe4ed107cb56faa2c0c57
Владимир Петько <petko.v@yandex.ru>
2022-01-03 14:42:55 +0600

    03_add_clientClass

---
+++ src/main/java/ru/vlpetko/metroservice/service/client/MosDataApiClient.java
@@ -0,0 +1,10 @@
+package ru.vlpetko.metroservice.service.client;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MosDataApiClient {
+    public void getDataFromOpenSource() {
+        System.out.println("new data");
+    }
+}
