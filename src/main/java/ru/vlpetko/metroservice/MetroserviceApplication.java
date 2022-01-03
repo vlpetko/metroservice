2600913c05764dcc68d53ffb97e569ceb8fee77e
Владимир Петько <petko.v@yandex.ru>
2022-01-03 12:55:30 +0600

    01

---
+++ src/main/java/ru/vlpetko/metroservice/MetroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.vlpetko.metroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vlpetko/metroservice/MetroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlpetko.metroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
