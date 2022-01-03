3d3fb155e0ced6467e66a0647c7b494de98a84d4
Владимир Петько <petko.v@yandex.ru>
2022-01-03 13:43:04 +0600

    02_add_checkstyle

--- src/main/java/ru/vlpetko/metroservice/MetroServiceApplication.java
+++ src/main/java/ru/vlpetko/metroservice/MetroServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetroserviceApplication {
+public class MetroServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MetroserviceApplication.class, args);
+        SpringApplication.run(MetroServiceApplication.class, args);
     }
 
 }
