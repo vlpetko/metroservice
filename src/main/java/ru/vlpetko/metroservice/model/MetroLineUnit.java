ed40d98e342013400d81df070f230df58faeab72
Владимир Петько <petko.v@yandex.ru>
2022-01-04 17:53:22 +0600

    add getData method

---
+++ src/main/java/ru/vlpetko/metroservice/model/MetroLineUnit.java
@@ -0,0 +1,11 @@
+package ru.vlpetko.metroservice.model;
+
+import lombok.Data;
+
+@Data
+public class MetroLineUnit {
+
+    private int number;
+    private MetroLine metroLine;
+
+}
--- src/main/java/ru/vlpetko/metroservice/service/client/MosDataApiClient.java
+++ src/main/java/ru/vlpetko/metroservice/service/client/MosDataApiClient.java
@@ -1,10 +1,55 @@
 package ru.vlpetko.metroservice.service.client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.vlpetko.metroservice.model.MetroLine;
+import ru.vlpetko.metroservice.model.MetroLineUnit;
+import ru.vlpetko.metroservice.service.client.dto.MetroLineDto;
+import ru.vlpetko.metroservice.service.client.dto.MetroLineUnitDtoJson;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MosDataApiClient {
-    public void getDataFromOpenSource() {
-        System.out.println("new data");
+
+    @Value("${apiLineUrl}")
+    String apiLineUrl;
+    @Value("${apiKey}")
+    String apiKey;
+
+    public List<MetroLineUnit> getDataFromOpenSource() {
+        List<MetroLineUnitDtoJson> resultJson;
+        List<MetroLineUnit> result = new ArrayList<>();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            resultJson = mapper.readValue(
+                    new URL(apiLineUrl + apiKey),
+                    new TypeReference<>() {
+                    });
+            for (int i = 0; i < resultJson.size(); i++) {
+                MetroLineUnitDtoJson unitDtoJson = resultJson.get(i);
+                MetroLineUnit unit = new MetroLineUnit();
+                unit.setNumber(unitDtoJson.getNumber());
+                MetroLine metroLine = new MetroLine();
+                MetroLineDto lineDto = unitDtoJson.getMetroLineDto();
+                metroLine.setLine(lineDto.getLine());
+                metroLine.setNumberOfStations(lineDto.getNumberOfStations());
+                metroLine.setMetroLineLength(lineDto.getMetroLineLength());
+                metroLine.setNumberOfCarriages(lineDto.getNumberOfCarriages());
+                unit.setMetroLine(metroLine);
+                result.add(unit);
+                System.out.println(unit);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+
     }
 }
